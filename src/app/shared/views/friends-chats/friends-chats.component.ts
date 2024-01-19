dd6a06cd838f09535d7cc68affb7b091de578d33
Carlos Adrián Ruiz Hernández <carh_080902@hotmail.es>
2024-01-19 13:41:02 -0600

    Lista de contactos: contactos mostrandose

--- src/app/shared/views/friends-chats/friends-chats.component.ts
+++ src/app/shared/views/friends-chats/friends-chats.component.ts
@@ -3,7 +3,7 @@ import { SessionService } from '../../services/session/session.service';
 import { FriendService } from '../../services/friend/friend.service';
 import { UserService } from '../../services/user/user.service';
 import { Friend } from '../../models/Friend';
-import { Subscription } from 'rxjs';
+import { Subscription, take } from 'rxjs';
 
 @Component({
   selector: 'app-friends-chats',
@@ -12,7 +12,7 @@ import { Subscription } from 'rxjs';
 })
 export class FriendsChatsComponent implements OnInit, OnDestroy {
 
-  protected friends_array: any = [];
+  protected friends_array: any;
   public current_session: any;
   private friends_subscription!: Subscription;
   protected is_friends_list_loaded: boolean = false;
@@ -23,36 +23,56 @@ export class FriendsChatsComponent implements OnInit, OnDestroy {
     private friend_service: FriendService
   ) {
     this.current_session = this.session_service.getSession();
+    this.friends_array = [];
   }
 
   ngOnInit(): void {
+    this.getFriendList();
+  }
+
+  getFriendList() {
     try {
-      this.friends_array = [];
-      this.friends_subscription = this.friend_service.getFriendList(this.current_session).subscribe((response) => {
-        if(response) {
-          response.forEach(async (friend) => {
-            if(friend.user_id_1 != this.current_session) {
-              let friend_user_object = await this.user_service.getUserById(friend.user_id_1);
-              this.friends_array.push(
-                {
-                  id: friend_user_object.id,
-                  data: friend_user_object.object
-                }
-              );
-            }
-            else {
-              let friend_user_object = await this.user_service.getUserById(friend.user_id_2)
-              this.friends_array.push(
-                {
-                  id: friend_user_object.id,
-                  data: friend_user_object.object
-                }
-              );
-            }
-          })
-          this.is_friends_list_loaded = true;
-        }
-      });
+      if(this.friends_subscription) {
+        this.unsubscribeFriendList();
+      }
+      else {
+        this.friends_subscription = this.friend_service.getFriendList(this.current_session).subscribe((response) => {
+          if(response) {
+            response.forEach(async (friend) => {
+              if(friend.user_id_1 != this.current_session) {
+                let friend_user_object = await this.user_service.getUserById(friend.user_id_1);
+                this.friends_array.push(
+                  {
+                    id: friend_user_object.id,
+                    data: friend_user_object.object
+                  }
+                  );
+              }
+              else {
+                let friend_user_object = await this.user_service.getUserById(friend.user_id_2)
+                this.friends_array.push(
+                  {
+                    id: friend_user_object.id,
+                    data: friend_user_object.object
+                  }
+                );
+              }
+            })
+            this.is_friends_list_loaded = true;
+            console.log('suscripcion abierta')
+          }
+        });
+      }
+    }
+    catch(error) {
+      console.log(error);
+    }
+  }
+
+  unsubscribeFriendList(){
+    try {
+      this.friends_subscription.unsubscribe();
+      console.log('suscripcion cerrada')
     }
     catch(error) {
       console.log(error);
@@ -60,13 +80,7 @@ export class FriendsChatsComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    try {
-      this.friends_array = [];
-      this.friends_subscription.unsubscribe();
-    }
-    catch(error) {
-      console.log(error);
-    }
+    this.unsubscribeFriendList();
   }
 
 }
